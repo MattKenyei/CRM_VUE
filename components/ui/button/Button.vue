10d442a4881678bf0c9ee87d4a155b77ab834cf8
MattKenyei <mattkenyei@gmail.com>
2024-06-24 01:55:03 +0300

    first commit. init project + created sidebar

---
+++ components/ui/button/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { cn } from '@/utils'
+import { buttonVariants } from './index'
+
+interface Props {
+	variant?: NonNullable<Parameters<typeof buttonVariants>[0]>['variant']
+	size?: NonNullable<Parameters<typeof buttonVariants>[0]>['size']
+	as?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
