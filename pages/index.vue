10c97b0e78eedad1c3b122068bb54633d05c1708
MattKenyei <mattkenyei@gmail.com>
2024-06-25 19:46:38 +0300

    added card template & started kanban

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-
+useHead({
+    title: 'Home | CRM System',
+})
 </script>
 <template>
     <div class="p-10">
         <h1 class="font-bold text-2xl mb-10">CRM System by MattKenyei</h1>
+    <div>
+        <UiCard class="mb-3" draggable="true">
+            <UiCardHeader role="button">name card</UiCardHeader>
+            <UiCardContent>Company</UiCardContent>
+            <UiCardFooter>Date</UiCardFooter>
+        </UiCard>
+    </div>
     </div>
 </template>
 
