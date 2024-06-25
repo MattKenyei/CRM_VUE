93fbb792ee8859acf9e0e11c1c24bcca9cdb61f5
MattKenyei <mattkenyei@gmail.com>
2024-06-26 02:28:30 +0300

    added transform and output data into cards

--- components/kanban/usekanbanQuery.ts
+++ components/kanban/usekanbanQuery.ts
@@ -9,8 +9,22 @@ export function UseKanbanQuery() {
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
     select(data) {
       const newBoard = [...KANBAN_DATA];
+      const deals = data.documents as unknown as IDeal[];
 
-      const deals = data.documents as unknown as IDeal[]
+      for (const deal of deals) {
+        const column = newBoard.find((col) => col.id === deal.status);
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name,
+          });
+        }
+      }
+      return newBoard;
     },
   });
 }
